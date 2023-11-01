04bea492926a371983763dbcfc644f6c5daec75b
Kishwar Jabeen <kishwerjabeen90@gmail.com>
2023-11-01 13:35:42 +0500

    iport imgaes

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from 'react';
+import image1 from '../../public/img/b1.jpg';
+import image2 from '../../public/img/b2.jpg';
+import image3 from '../../public/img/b3.jpg';
+import image4 from '../../public/img/b4.jpg';
 
 const Carousel = () => {
   return (
